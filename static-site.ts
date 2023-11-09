5e8ddddcaa876e4da8fab25aa8a8b61e744cc23f
hs-himzheuski <vitali.himzheuski@headspace.com>
2023-11-09 19:07:11 +0100

    add service

--- static-site.ts
+++ static-site.ts
@@ -11,6 +11,38 @@ export class StaticSite extends Construct {
   constructor(parent: Stack, name: string) {
     super(parent, name);
 
-    
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
+
+    const siteBucket = new s3.Bucket(this, "JSCCStaticBucket", {
+      bucketName: "jimmba-js-cc-cloudfront-s3",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    });
+
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ["S3:GetObject"],
+      resources: [siteBucket.arnForObjects("*")],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, "JSCC-distribution", {
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: siteBucket,
+          originAccessIdentity: cloudfrontOAI
+        },
+        behaviors: [{
+          isDefaultBehavior: true,
+        }]
+      }]
+    })
+
+    new s3deploy.BucketDeployment(this, "JSCC-BucketDeployment", {
+      sources: [s3deploy.Source.asset("./website")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"]
+    })
   }
 }
